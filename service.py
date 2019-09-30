cef385c1ce4a5a52abe2f801100d9aeb074aa5c6
wilkinsb <brandwilki@gmail.com>
2019-09-30 11:15:16 -0700

    add all local function launch tools

--- service.py
+++ service.py
@@ -25,7 +25,7 @@ chmod 0400 .github_ssh_key
 
 export GIT_SSH_COMMAND="ssh -i /.github_ssh_key -o StrictHostKeyChecking=no"
 
-git clone git@github.com:sciscogenetics/docker-packer.git
+git clone git@github.com:wilkinsb/docker-packer.git
 cd docker-packer/scripts
 
 ./build-docker.sh
@@ -35,7 +35,32 @@ cd docker-packer/scripts
     return output
 
 
-def trigger_build():
+def find_image():
+    """
+    Finds the latest Canonical Ubuntu 16.04 AMI and return the ID.
+    based on
+    https://gist.github.com/robert-mcdermott/a9901aaafe208a6eb76e0fc3b9fc47c9
+    """
+    ec2 = boto3.resource('ec2', region_name="us-west-2")
+    images = ec2.images.filter(
+        Owners=['099720109477'],
+        Filters=[
+            {'Name': 'name', 'Values': [
+                'ubuntu/images/hvm-ssd/ubuntu-xenial-16.04-amd64-server-*']},
+            {'Name': 'state', 'Values': ['available']},
+            {'Name': 'architecture', 'Values': ['x86_64']},
+            {'Name': 'root-device-type', 'Values': ['ebs']},
+            {'Name': 'virtualization-type', 'Values': ['hvm']}
+        ]
+    )
+    candidates = {}
+    for image in images:
+        candidates[image.creation_date] = image.image_id
+    cdate = sorted(candidates.keys(), reverse=True)[0]
+    ami = candidates[cdate]
+    return ami
+
+def trigger_build(event, context):
     """
     Both web and S3 events will eventually call this.
     """
@@ -43,7 +68,7 @@ def trigger_build():
 
     arn = conf.EC2_ARN
     response = ec2_client.run_instances(
-        # ImageId=find_image("us-west-2"),
+        ImageId=find_image(),
         MinCount=1,
         MaxCount=1,
         KeyName=conf.SSH_KEYNAME,
@@ -59,6 +84,3 @@ def trigger_build():
     instance_id = response["Instances"][0]["ImageId"]
     return instance_id
 
-
-if __name__ == "__main__":
-    trigger_build()
