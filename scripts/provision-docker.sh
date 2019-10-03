012ec740d7e0ae54abfee21868c04ea8e3fcb57b
wilkinsb <brandwilki@gmail.com>
2019-10-03 13:40:05 -0700

    add in provisioning code to include pipeline

--- scripts/provision-docker.sh
+++ scripts/provision-docker.sh
@@ -8,37 +8,46 @@ set -x
 set -v -v -v -v
 set -e # exit if any command fails
 
-# apt-get update
-# apt-get install -y git
+apt-get update
+apt-get install -y git
 
-# cd /
+cd /
+echo $GITHUB_SSH_KEY | sudo sed 's/NEWLINE/\n/g' > /home/ubuntu/.github_ssh_key
+sudo mv /home/ubuntu/.github_ssh_key /
+sudo chmod 0400 /.github_ssh_key
+
+export GIT_SSH_COMMAND="ssh -i /.github_ssh_key -o StrictHostKeyChecking=no"
+
+cd /
+git clone git@github.com:sciscogenetics/gems-pipeline.git
+cd gems-pipeline
+git checkout staging
+
+cd /
+
+set -x
+set -v -v -v -v
+set -e # exit if any command fails
 
 
-# set -x
-# set -v -v -v -v
-# set -e # exit if any command fails
+# apt-get update # not needed, already done
+apt-get install -y openjdk-8-jdk-headless \
+python libcurl4-openssl-dev libssl-dev vim curl \
+build-essential python-dev
+apt-get remove -y unattended-upgrades liblxc1 lxd-client lxcfs
 
+curl https://bootstrap.pypa.io/get-pip.py | python
 
-# # apt-get update # not needed, already done
-# apt-get install -y openjdk-8-jdk-headless \
-# python libcurl4-openssl-dev libssl-dev vim curl \
-# build-essential python-dev
-
-# apt-get remove -y unattended-upgrades liblxc1 lxd-client lxcfs
-
-# curl https://bootstrap.pypa.io/get-pip.py | python
-
-# pip install -U pip setuptools
-
-# pip install virtualenv awscli ipython six
+pip install -U pip setuptools
+pip install virtualenv awscli ipython six
 
 
 cd /
 
+# This is where we provision our DIST CODE PACKAGES using awscli
+
 echo $ECR_URI
 echo $ECR_LOGIN_SERVER
-echo $AWS_ACCESS_KEY
-echo $AWS_SECRET_KEY
 
 
 echo "python and java installed successfully"
